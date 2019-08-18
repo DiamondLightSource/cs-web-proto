e4fc48e30dd272474afbe4e1c7957bb565ea9492
Will Rogers <wahrogers@gmail.com>
2019-08-18 11:36:01 +0100

    Fix readback test.

--- src/components/readback.test.tsx
+++ src/components/readback.test.tsx
@@ -5,15 +5,13 @@ import renderer from "react-test-renderer";
 
 test("snapshot matches", (): void => {
   const readback = renderer.create(
-    <Readback pvName={"mypv"} value={{ type: "blah", value: "hello" }} />
+    <Readback pvName={"mypv"} value={"hello"} />
   );
   let json = readback.toJSON();
   expect(json).toMatchSnapshot();
 });
 
 it("renders a basic element", (): void => {
-  const wrapper = shallow(
-    <Readback pvName={"mypv"} value={{ type: "blah", value: "hello" }} />
-  );
+  const wrapper = shallow(<Readback pvName={"mypv"} value={"hello"} />);
   expect(wrapper.text()).toEqual("mypv: hello");
 });
