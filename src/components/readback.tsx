5c68fc9d2e4931b27248f6c3611cbadde5b2b514
Will Rogers <wahrogers@gmail.com>
2019-08-18 11:15:26 +0100

    Remove unused import.

--- src/components/readback.tsx
+++ src/components/readback.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useSubscription } from "../hooks/useCs";
-import { NType } from "../cs";
 import { CsState } from "../redux/store";
 
 export const Readback = (props: {
