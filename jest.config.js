30513aa858b0ff702de5e05c2d24cce5fba823cb
Will Rogers <will.rogers@diamond.ac.uk>
2019-08-19 16:30:15 +0100

    Add coverage to npm test command.

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   snapshotSerializers: ["enzyme-to-json/serializer"],
-  setupTestFrameworkScriptFile: "<rootDir>/src/setupEnzyme.ts"
+  setupTestFrameworkScriptFile: "<rootDir>/src/setupTests.ts",
+  collectCoverage: true,
+  collectCoverageFrom: ["**/*.ts", "!<rootDir>/node_modules/**"]
+  coverageReporters: ["lcov", "text-summary", "html"]
 };
