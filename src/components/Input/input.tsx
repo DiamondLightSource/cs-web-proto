91cc6cfba2f771089aa931699e6da047b833cdc0
Tim Guite <tim.guite@diamond.ac.uk>
2019-08-21 11:53:54 +0100

    Change from locale string to normal to string (seems to be difference between our locale and Travis's locale causing problem)

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -18,7 +18,7 @@ export const Input: React.FC<InputProps> = (props: InputProps): JSX.Element => (
   <BaseWidget
     pvName={props.pvName}
     value={props.value}
-    timestamp={new Date(1566377886000).toLocaleString()}
+    timestamp={new Date(1566377886000).toString()}
   >
     <input
       type="text"
