b4705fb9b3e434e0f88a572b2f9ad3f14e2e8488
Tim Guite <tim.guite@diamond.ac.uk>
2019-08-27 14:37:06 +0100

    Changed App to make it easier to differentiate elements

--- src/App.tsx
+++ src/App.tsx
@@ -20,87 +20,97 @@ const App: React.FC = (): JSX.Element => {
   return (
     <Provider store={store}>
       <div className="App">
-        <h1>CS Web Proto</h1>
-        <div style={{ display: "block" }}>
-          <ConnectedReadback pvName={"TMC43-TS-IOC-01:AI"} />
-          <ConnectedReadback pvName={"loc://pv1"} />
-          <ConnectedReadback pvName={"loc://pv2"} />
-          <ConnectedReadback pvName={"sim://sine"} precision={3} />
-          <ConnectedReadback pvName={"sim://disconnector"} precision={3} />
-        </div>
-        <div style={{ display: "block" }}>
-          <ConnectedInput pvName={"loc://pv1"} />
-          <ConnectedInput pvName={"loc://pv2"} />
-          <ConnectedInput pvName={"sim://sine"} />
-          <ConnectedInput pvName={"sim://sine"} />
-        </div>
-        <div>
-          <h3>PV with Metadata</h3>
-          <div style={{ display: "flex" }}>
-            <div style={{ flexGrow: 1 }}>
-              <ConnectedInput pvName={"meta://metapv1"} />
-            </div>
-            <div style={{ flexGrow: 1 }}>
-              <ConnectedCopyReadback pvName={"meta://metapv1"} />
+        <div id="Central Column" style={{ width: "80%", margin: "auto" }}>
+          <h1>CS Web Proto</h1>
+          <div style={{ display: "block" }}>
+            <ConnectedReadback pvName={"TMC43-TS-IOC-01:AI"} />
+            <ConnectedReadback pvName={"loc://pv1"} />
+            <ConnectedReadback pvName={"loc://pv2"} />
+            <ConnectedReadback pvName={"sim://sine"} precision={3} />
+            <ConnectedReadback pvName={"sim://disconnector"} precision={3} />
+          </div>
+          <div style={{ display: "block" }}>
+            <ConnectedInput pvName={"loc://pv1"} />
+            <ConnectedInput pvName={"loc://pv2"} />
+            <ConnectedInput pvName={"sim://sine"} />
+            <ConnectedInput pvName={"sim://sine"} />
+          </div>
+          <div>
+            <h3>PV with Metadata</h3>
+            <div
+              style={{
+                position: "relative",
+                display: "block",
+                height: "30px",
+                margin: "15px"
+              }}
+            >
+              <div
+                style={{
+                  display: "inline-block",
+                  position: "absolute",
+                  top: "0%",
+                  right: "50%",
+                  height: "100%",
+                  width: "50%",
+                  margin: "auto"
+                }}
+              >
+                <ConnectedInput pvName={"meta://metapv1"} />
+              </div>
+              <div
+                style={{
+                  display: "inline-block",
+                  position: "absolute",
+                  top: "0%",
+                  left: "50%",
+                  height: "100%",
+                  width: "50%",
+                  margin: "auto"
+                }}
+              >
+                <ConnectedReadback pvName={"meta://metapv1"} />
+              </div>
             </div>
           </div>
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            top: "500px",
-            left: "10%",
-            height: "20%",
-            width: "50%"
-          }}
-        >
-          <ConnectedSlideControl pvName="loc://pv1" min={0} max={100} />
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            left: "10%",
-            height: "20%",
-            width: "50%"
-          }}
-        >
-          <ConnectedProgressBar
-            pvName={"sim://sine"}
-            min={-1}
-            max={1}
-            precision={2}
-          />
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            top: "30%",
-            left: "70%",
-            height: "20%",
-            width: "20%"
-          }}
-        >
-          <ConnectedCopyWrapper pvName="meta://metapv1">
-            This is a connected copy wrapper
-          </ConnectedCopyWrapper>
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            top: "55%",
-            left: "70%",
-            height: "20%",
-            width: "20%"
-          }}
-        >
-          <AlarmBorder alarm={{ severity: 2, status: 0, message: "" }}>
-            <ConnectedProgressBar
-              pvName={"sim://sine"}
-              min={-1}
-              max={1}
-              precision={2}
+          <div
+            style={{
+              position: "relative",
+              height: "200px",
+              margin: "15px auto"
+            }}
+          >
+            <ConnectedProgressBar pvName={"meta://metapv1"} min={0} max={100} />
+          </div>
+          <div
+            style={{
+              position: "relative",
+              height: "200px",
+              margin: "15px auto"
+            }}
+          >
+            <ConnectedSlideControl
+              pvName={"meta://metapv1"}
+              min={0}
+              max={100}
             />
-          </AlarmBorder>
+          </div>
+          <div
+            style={{
+              position: "relative",
+              height: "200px",
+              margin: "15px auto"
+            }}
+          >
+            <AlarmBorder alarm={{ severity: 2, status: 0, message: "" }}>
+              <ConnectedProgressBar
+                pvName={"sim://sine"}
+                min={-1}
+                max={1}
+                precision={2}
+              />
+            </AlarmBorder>
+          </div>
         </div>
       </div>
     </Provider>
