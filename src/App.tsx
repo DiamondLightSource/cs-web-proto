ea8de0b90cdf565b7cff28539f794f8c3bfcd069
Tim Guite <tim.guite@diamond.ac.uk>
2019-09-02 12:08:20 +0100

    Colorful example

--- src/App.tsx
+++ src/App.tsx
@@ -15,136 +15,13 @@ import { AlarmBorder } from "./components/AlarmBorder/AlarmBorder";
 import { getStore, initialiseStore } from "./redux/store";
 import { SimulatorPlugin } from "./connection/sim";
 
+import { Mapping } from "./components/Positioning/positioningExample";
+
 const App: React.FC = (): JSX.Element => {
   const plugin = new SimulatorPlugin();
   initialiseStore(plugin);
   let store = getStore();
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <div id="Central Column" style={{ width: "80%", margin: "auto" }}>
-          <h1>CS Web Proto</h1>
-          <div style={{ display: "block" }}>
-            <ConnectedReadback pvName={"TMC43-TS-IOC-01:AI"} />
-            <ConnectedReadback pvName={"loc://pv1"} />
-            <ConnectedReadback pvName={"loc://pv2"} />
-            <ConnectedReadback pvName={"sim://sine"} precision={3} />
-            <ConnectedReadback pvName={"sim://disconnector"} precision={3} />
-          </div>
-          <div style={{ display: "block" }}>
-            <ConnectedInput pvName={"loc://pv1"} />
-            <ConnectedInput pvName={"loc://pv2"} />
-            <ConnectedInput pvName={"sim://sine"} />
-            <ConnectedInput pvName={"sim://sine"} />
-          </div>
-          <div>
-            <h3>PV with Metadata</h3>
-            <div
-              style={{
-                position: "relative",
-                display: "block",
-                height: "30px",
-                margin: "15px"
-              }}
-            >
-              <div
-                style={{
-                  display: "inline-block",
-                  position: "absolute",
-                  top: "0%",
-                  right: "50%",
-                  height: "100%",
-                  width: "50%",
-                  margin: "auto"
-                }}
-              >
-                <ConnectedInput pvName={"meta://metapv1"} />
-              </div>
-              <div
-                style={{
-                  display: "inline-block",
-                  position: "absolute",
-                  top: "0%",
-                  left: "50%",
-                  height: "100%",
-                  width: "50%",
-                  margin: "auto"
-                }}
-              >
-                <ConnectedReadback pvName={"meta://metapv1"} />
-              </div>
-            </div>
-            <div
-              style={{
-                position: "relative",
-                height: "2em",
-                margin: "15px auto"
-              }}
-            >
-              <ConnectedStandaloneReadback
-                pvName={"meta://metapv1"}
-                precision={2}
-              />
-            </div>
-            <div
-              style={{
-                position: "relative",
-                height: "2em",
-                margin: "15px auto"
-              }}
-            >
-              <ConnectedStandaloneReadback
-                pvName={"sim://disconnector"}
-                precision={2}
-              />
-            </div>
-          </div>
-          <div
-            style={{
-              position: "relative",
-              height: "200px",
-              margin: "15px auto"
-            }}
-          >
-            <ConnectedStandaloneProgressBar
-              pvName={"meta://metapv1"}
-              min={0}
-              max={100}
-            />
-          </div>
-          <div
-            style={{
-              position: "relative",
-              height: "200px",
-              margin: "15px auto"
-            }}
-          >
-            <ConnectedSlideControl
-              pvName={"meta://metapv1"}
-              min={0}
-              max={100}
-            />
-          </div>
-          <div
-            style={{
-              position: "relative",
-              height: "200px",
-              margin: "15px auto"
-            }}
-          >
-            <AlarmBorder connected={false}>
-              <ConnectedProgressBar
-                pvName={"sim://sine"}
-                min={-1}
-                max={1}
-                precision={2}
-              />
-            </AlarmBorder>
-          </div>
-        </div>
-      </div>
-    </Provider>
-  );
+  return <Mapping />;
 };
 
 export default App;
---
+++ src/components/Positioning/positioningExample.tsx
@@ -0,0 +1,172 @@
+import React from "react";
+
+import { PositionDescription, objectToPosition } from "./positioning";
+
+const Blank: React.FC = (props: any): JSX.Element => (
+  <div
+    style={{
+      position: "absolute",
+      height: "100%",
+      width: "100%",
+      border: "solid 5px black"
+    }}
+  >
+    {props.children}
+  </div>
+);
+
+const RedBlock: React.FC = (props: any): JSX.Element => {
+  let borderStyle = {};
+  if (props.withBorder) {
+    borderStyle = {
+      border: "solid 5px pink"
+    };
+  }
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        height: "100%",
+        width: "100%",
+        backgroundColor: "red",
+        ...borderStyle
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+const BlueBlock: React.FC = (props: any): JSX.Element => (
+  <div
+    style={{
+      position: "absolute",
+      height: "100%",
+      width: "100%",
+      backgroundColor: "blue"
+    }}
+  >
+    {props.children}
+  </div>
+);
+
+const GreenBlock: React.FC = (props: any): JSX.Element => (
+  <div
+    style={{
+      position: "absolute",
+      height: "100%",
+      width: "100%",
+      backgroundColor: "green"
+    }}
+  >
+    {props.children}
+  </div>
+);
+
+const exampleDict: { [index: string]: React.FC } = {
+  "Red Block": RedBlock,
+  "Blue Block": BlueBlock,
+  "Green Block": GreenBlock,
+  Blank: Blank
+};
+
+const PosOne: PositionDescription = {
+  type: "Red Block",
+  x: "25%",
+  y: "25%",
+  width: "50%",
+  height: "50%",
+  withBorder: true
+};
+
+const PosTwo: PositionDescription = {
+  type: "Green Block",
+  x: "25%",
+  y: "25%",
+  width: "50%",
+  height: "50%",
+  children: [PosOne]
+};
+
+const PosThree: PositionDescription = {
+  type: "Blue Block",
+  x: 0,
+  y: 0,
+  width: "100%",
+  height: "100%",
+  children: [PosTwo]
+};
+
+const MapOne: PositionDescription = {
+  type: "Blank",
+  x: 0,
+  y: 0,
+  width: "100%",
+  height: "100%",
+  children: [
+    {
+      type: "Blank",
+      x: 0,
+      y: 0,
+      width: "50%",
+      height: "100%",
+      children: [PosThree]
+    },
+    {
+      type: "Blank",
+      x: "50%",
+      y: 0,
+      width: "50%",
+      height: "100%",
+      children: [
+        {
+          type: "Blank",
+          x: 0,
+          y: 0,
+          width: "100%",
+          height: "50%",
+          children: [
+            {
+              type: "Red Block",
+              x: 0,
+              y: 0,
+              width: "33%",
+              height: "100%",
+              withBorder: true
+            },
+            {
+              type: "Green Block",
+              x: "33%",
+              y: 0,
+              width: "33%",
+              height: "100%"
+            },
+            {
+              type: "Blue Block",
+              x: "66%",
+              y: 0,
+              width: "34%",
+              height: "100%"
+            }
+          ]
+        },
+        {
+          type: "Blank",
+          x: 0,
+          y: "50%",
+          width: "100%",
+          height: "50%",
+          children: [PosThree]
+        }
+      ]
+    }
+  ]
+};
+
+export const Mapping: React.FC = (): JSX.Element => {
+  let Mapped = objectToPosition(MapOne, exampleDict);
+  console.log(Mapped);
+
+  return <div>{Mapped}</div>;
+};
