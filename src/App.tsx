bf19bce89f82f3031bd13a2f40e897a0732798f2
Tim Guite <tim.guite@diamond.ac.uk>
2019-09-16 10:27:44 +0100

    Added filename linting to provide consistent camelCase file names Added quotes to lint glob to avoid inconsistencies in which files are linted. This should help with other rules as well.
    Added the eslint-plugin-unicorn package as their filename linter is better than eslint-plugin-filename (it detects *.test.tsx files properly). Also contains a multitude of other rules which could be useful for us.
    
    Specifically disabled this eslint rule in App.tsx, App.test.tsx, react-app-env.d.ts which are files set up by create-react-app. Used the mechanism described here: https://eslint.org/docs/user-guide/configuring.html#configuring-rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+// Specifically disable some eslint rules
+/* eslint unicorn/filename-case: 0 */ // --> OFF
+
 import React from "react";
 import "./App.css";
 import { Provider } from "react-redux";
