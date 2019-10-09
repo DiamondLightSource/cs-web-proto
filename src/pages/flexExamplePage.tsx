f5c1e4de1708ba5316fe893d9f0ac17432a1a2eb
Tim Guite <tim.guite@diamond.ac.uk>
2019-10-09 12:01:17 +0100

    Converted flex example page to be generated directly from JSON

--- src/pages/flexExamplePage.tsx
+++ src/pages/flexExamplePage.tsx
@@ -1,18 +1,10 @@
 // Page to show off the absolute positiong example
 import React from "react";
 
+import { FromJson } from "../components/FromJson/fromJson";
+
 import { Mapping } from "../components/Positioning/flexExample";
 
 export const FlexExamplePage = (): JSX.Element => (
-  <div
-    style={{
-      position: "relative",
-      height: "100%",
-      width: "100%",
-      textAlign: "center"
-    }}
-  >
-    <h2>Abolute Positioning Example</h2>
-    <Mapping />
-  </div>
+  <FromJson file="http://localhost:3000/flexiblePage.json" />
 );
