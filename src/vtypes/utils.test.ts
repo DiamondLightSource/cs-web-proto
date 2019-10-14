242a0b2b62019d38ed5af5ee1032b378d385e14d
Tim Guite <tim.guite@diamond.ac.uk>
2019-10-14 14:18:26 +0100

    Fixed linting

--- src/vtypes/utils.test.ts
+++ src/vtypes/utils.test.ts
@@ -1,11 +1,9 @@
 import { stringToVtype } from "./utils";
 
-import { VType } from "./vtypes";
-
-test("stringToVType returns string from string", () => {
+test("stringToVType returns string from string", (): void => {
   expect(stringToVtype("This is a string").getValue()).toBe("This is a string");
 });
 
-test("stringToVType returns number from number in string", () => {
+test("stringToVType returns number from number in string", (): void => {
   expect(stringToVtype("3.14159").getValue()).toBe(3.14159);
 });
