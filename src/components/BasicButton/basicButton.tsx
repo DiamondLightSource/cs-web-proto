a6ce6f39c3739c7177a30d405aafff1091959f67
Sky French <sky.french@diamond.ac.uk>
2019-10-29 09:07:15 +0000

    image button

--- src/components/BasicButton/basicButton.tsx
+++ src/components/BasicButton/basicButton.tsx
@@ -1,24 +1,43 @@
 import React from "react";
 import { PVWidget, PVWidgetInterface } from "../Widget/widget";
+import classes from "./basicButton.module.css";
 
 export interface BasicButtonProps {
   text: string;
   style?: {};
+  image?: string;
 }
 
 export const BasicButtonComponent = (props: BasicButtonProps): JSX.Element => {
-  return <button>{props.text}</button>;
+  if (props.image !== undefined) {
+    return (
+      <button className={classes.image}>
+        <img src={props.image}></img>
+        <br></br>
+        {props.text}
+      </button>
+    );
+  } else {
+    return <button>{props.text}</button>;
+  }
 };
 
 export interface BasicButtonWidgetProps {
   text: string;
   style?: {};
+  image?: string;
 }
 
 export const BasicButtonWidget = (
   props: BasicButtonWidgetProps
 ): JSX.Element => {
-  return <BasicButtonComponent text={props.text} style={props.style} />;
+  return (
+    <BasicButtonComponent
+      text={props.text}
+      style={props.style}
+      image={props.image}
+    />
+  );
 };
 
 export const BasicButton = (
