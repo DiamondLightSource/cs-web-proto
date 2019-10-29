a096b4269fbdbc037b7c6c35a9b1c5f03afbf967
Sky French <sky.french@diamond.ac.uk>
2019-10-29 15:09:52 +0000

    further styling tweaks

--- src/components/BasicButton/basicButton.tsx
+++ src/components/BasicButton/basicButton.tsx
@@ -11,13 +11,11 @@ export interface BasicButtonProps {
 export const BasicButtonComponent = (props: BasicButtonProps): JSX.Element => {
   if (props.image !== undefined) {
     return (
-      <div style={props.style}>
-        <button className={classes.image}>
-          <img src={props.image} alt={props.image}></img>
-          <br></br>
-          {props.text}
-        </button>
-      </div>
+      <button className={classes.image}>
+        <img src={props.image} alt={props.image}></img>
+        <br></br>
+        {props.text}
+      </button>
     );
   } else {
     return <button>{props.text}</button>;
