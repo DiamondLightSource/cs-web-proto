6d61467799575d0182fb8f63f6e2d16f328e3a43
Sky French <sky.french@diamond.ac.uk>
2019-10-31 09:51:16 +0000

    beginning context menu test

---
+++ src/components/MenuWrapper/menuWrapper.test.tsx
@@ -0,0 +1,33 @@
+import React, { Children } from "react";
+import { shallow, ShallowWrapper } from "enzyme";
+
+import { Items, MenuWrapper } from "./menuWrapper";
+
+let wrapper: ShallowWrapper;
+let wrappedElement: ShallowWrapper;
+
+beforeEach((): void => {
+  let item: Items = {
+    actions: { executeAsOne: false, actions: [] },
+    label: "empty"
+  };
+  let items: Items[] = [item];
+  const menuWrapper = (
+    <MenuWrapper pvName="pv" items={items}>
+      Children
+    </MenuWrapper>
+  );
+  wrapper = shallow(menuWrapper);
+  wrappedElement = wrapper.find("div").childAt(0);
+});
+
+describe("<MenuWrapper>", (): void => {
+  test("it renders a basic element", (): void => {
+    expect(wrapper.text()).toContain("Children");
+  });
+  test("it contains one child element", (): void => {
+    const children = wrapper.find("div");
+    expect(children).toHaveLength(1);
+    expect(children.find("div")).toHaveLength(1);
+  });
+});
