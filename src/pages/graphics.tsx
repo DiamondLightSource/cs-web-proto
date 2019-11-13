e7414a46f923b38d5f2b623fc123392770a9778e
Sky French <sky.french@diamond.ac.uk>
2019-11-13 13:04:24 +0000

    Revert "remove unused graphics page/link"
    This reverts commit 6b22c5db7225399e0df08460c0a91961571d84f8.

---
+++ src/pages/graphics.tsx
@@ -0,0 +1,17 @@
+// Page to show off graphical layouts
+import React from "react";
+
+import { WidgetFromJson } from "../components/FromJson/fromJson";
+
+export const GraphicalPage = (): JSX.Element => (
+  <WidgetFromJson
+    file="http://localhost:3000/inputsPage.json"
+    containerStyling={{
+      position: "relative",
+      height: "",
+      width: "",
+      margin: "",
+      padding: ""
+    }}
+  />
+);
