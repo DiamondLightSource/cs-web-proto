805f56415f83d79b3e6ea83cd3f41c762d73d80c
Tim Guite <tim.guite@diamond.ac.uk>
2019-11-25 10:56:38 +0000

    Added Grouping Container component and widget similar to Phoebus styling

---
+++ src/components/GroupingContainer/groupingContainer.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import {
+  Component,
+  Widget,
+  WidgetPropType,
+  InferWidgetProps
+} from "../Widget/widget";
+
+const GroupingContainerProps = {
+  name: PropTypes.string.isRequired,
+  children: PropTypes.node
+};
+
+// Generic display widget to put other things inside
+const GroupingContainerComponent = (
+  props: InferWidgetProps<typeof GroupingContainerProps> & Component
+): JSX.Element => (
+  // Uses an inner margin for children similar to Phoebus
+  // This prevents the title being overwritten
+  // Could be changed or perhaps customisable as a prop
+  <fieldset style={props.style}>
+    <legend>{props.name}</legend>
+    <div style={{ position: "absolute", top: "20px", left: "20px" }}>
+      {props.children}
+    </div>
+  </fieldset>
+);
+
+const GroupingWidgetProps = {
+  ...GroupingContainerProps,
+  ...WidgetPropType
+};
+
+export const GroupingContainer = (
+  props: InferWidgetProps<typeof GroupingWidgetProps>
+): JSX.Element => <Widget baseWidget={GroupingContainerComponent} {...props} />;
+
+GroupingContainer.propTypes = GroupingWidgetProps;
