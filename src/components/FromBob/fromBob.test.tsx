7d1e07e71f1e94c971728d75c85fdd85923d6d07
Tim Guite <tim.guite@diamond.ac.uk>
2019-11-28 11:21:41 +0000

    Fixed test to convert bob files
    Tested some simple cases

--- src/components/FromBob/fromBob.test.tsx
+++ src/components/FromBob/fromBob.test.tsx
@@ -3,6 +3,7 @@ import { WidgetFromBob } from "./fromBob";
 import { shallow } from "enzyme";
 
 import { Display } from "../Display/display";
+import { Label } from "../Label/label";
 
 describe("<WidgetFromBob>", (): void => {
   it("fetches a file from the server", (done): void => {
@@ -41,6 +42,115 @@ describe("<WidgetFromBob>", (): void => {
     process.nextTick((): void => {
       // 6
       expect(wrapper.type()).toEqual(Display);
+
+      globalWithFetch.fetch.mockClear(); // 7
+      done(); // 8
+    });
+  });
+  it("converts a simple widget", (done): void => {
+    const mockSuccessResponse = `
+    <widget type="label" version="2.0.0">
+        <name>Label</name>
+        <text>From .bob file</text>
+        <x>30</x>
+        <y>10</y>
+        <width>140</width>
+    </widget>`;
+    const mockTextPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+      text: (): Promise<{}> => mockTextPromise
+    });
+
+    // Hack to satisfy typescript
+    interface GlobalFetch extends NodeJS.Global {
+      fetch: any;
+    }
+    const globalWithFetch = global as GlobalFetch;
+
+    jest
+      .spyOn(global as GlobalFetch, "fetch")
+      .mockImplementation((): Promise<{}> => mockFetchPromise);
+
+    const wrapper = shallow(
+      <WidgetFromBob
+        containerStyling={{
+          position: "relative",
+          height: "",
+          width: "",
+          margin: "",
+          padding: ""
+        }}
+        file="TestFile"
+      />
+    );
+
+    expect(globalWithFetch.fetch).toHaveBeenCalledTimes(1);
+    expect(globalWithFetch.fetch).toHaveBeenCalledWith("TestFile");
+
+    process.nextTick((): void => {
+      // 6
+      expect(wrapper.type()).toEqual(Label);
+
+      globalWithFetch.fetch.mockClear(); // 7
+      done(); // 8
+    });
+  });
+
+  it("converts a display with child widget", (done): void => {
+    const mockSuccessResponse = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <display version="2.0.0">
+        <name>Display</name><width>200</width>
+        <height>350</height>
+        <widget type="label" version="2.0.0">
+            <name>Label</name>
+            <text>From .bob file</text>
+            <x>30</x>
+            <y>10</y>
+            <width>140</width>
+        </widget>
+    </display>`;
+    const mockTextPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+      text: (): Promise<{}> => mockTextPromise
+    });
+
+    // Hack to satisfy typescript
+    interface GlobalFetch extends NodeJS.Global {
+      fetch: any;
+    }
+    const globalWithFetch = global as GlobalFetch;
+
+    jest
+      .spyOn(global as GlobalFetch, "fetch")
+      .mockImplementation((): Promise<{}> => mockFetchPromise);
+
+    const wrapper = shallow(
+      <WidgetFromBob
+        containerStyling={{
+          position: "relative",
+          height: "",
+          width: "",
+          margin: "",
+          padding: ""
+        }}
+        file="TestFile"
+      />
+    );
+
+    expect(globalWithFetch.fetch).toHaveBeenCalledTimes(1);
+    expect(globalWithFetch.fetch).toHaveBeenCalledWith("TestFile");
+
+    process.nextTick((): void => {
+      // 6
+      expect(wrapper.type()).toEqual(Display);
+      expect(wrapper.childAt(0).type()).toEqual(Display);
+      expect(
+        wrapper
+          .childAt(0)
+          .childAt(0)
+          .type()
+      ).toEqual(Label);
 
       globalWithFetch.fetch.mockClear(); // 7
       done(); // 8
