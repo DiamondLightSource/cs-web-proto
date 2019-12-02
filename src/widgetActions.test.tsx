9c02f884f90ecc0f5929a507dddb7813691151ee
Will Rogers <will.rogers@diamond.ac.uk>
2019-12-02 17:20:09 +0000

    Further tests for widget actions.

--- src/widgetActions.test.tsx
+++ src/widgetActions.test.tsx
@@ -1,4 +1,17 @@
-import { WRITE_PV, getActionDescription, WritePv } from "./widgetActions";
+import {
+  WRITE_PV,
+  getActionDescription,
+  WritePv,
+  executeActions,
+  WidgetActions
+} from "./widgetActions";
+import { writePv } from "./hooks/useSubscription";
+
+jest.mock("./hooks/useSubscription", (): object => {
+  return {
+    writePv: jest.fn()
+  };
+});
 
 const WRITE_PV_ACTION: WritePv = {
   type: WRITE_PV,
@@ -13,6 +26,16 @@ const WRITE_PV_ACTION_NO_DESC: WritePv = {
   value: "value"
 };
 
+const ACTIONS_EX_AS_ONE = {
+  actions: [WRITE_PV_ACTION, WRITE_PV_ACTION_NO_DESC],
+  executeAsOne: true
+};
+
+const ACTIONS_EX_FIRST = {
+  actions: [WRITE_PV_ACTION, WRITE_PV_ACTION_NO_DESC],
+  executeAsOne: false
+};
+
 describe("getActionDescription", (): void => {
   it("returns description if present", (): void => {
     const desc = getActionDescription(WRITE_PV_ACTION);
@@ -21,5 +44,15 @@ describe("getActionDescription", (): void => {
   it("generates description if not present", (): void => {
     const desc = getActionDescription(WRITE_PV_ACTION_NO_DESC);
     expect(desc).toEqual("Write value to PV");
+  });
+});
+
+describe("executeActions", (): void => {
+  it.each<[WidgetActions, number]>([
+    [ACTIONS_EX_AS_ONE, 2],
+    [ACTIONS_EX_FIRST, 3] // cumulative? we should reset it
+  ])("executes %d actions", (actions, num): void => {
+    executeActions(actions);
+    expect(writePv).toHaveBeenCalledTimes(num);
   });
 });
