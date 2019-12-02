8e8daba892ddb4d1df99824ff6e8303df8cf7bd5
Will Rogers <will.rogers@diamond.ac.uk>
2019-12-02 12:30:13 +0000

    Handle actions objects with zero length.

--- src/widgetActions.ts
+++ src/widgetActions.ts
@@ -61,14 +61,16 @@ export const executeAction = (action: WidgetAction): void => {
 };
 
 export const executeActions = (actions: WidgetActions): void => {
-  log.debug(`executing an action ${actions.actions[0].type}`);
-  let toExecute: WidgetAction[] = [];
-  if (actions.executeAsOne) {
-    toExecute = actions.actions;
-  } else {
-    toExecute = [actions.actions[0]];
-  }
-  for (const action of toExecute) {
-    executeAction(action);
+  if (actions.actions.length > 0) {
+    let toExecute: WidgetAction[] = [];
+    if (actions.executeAsOne) {
+      toExecute = actions.actions;
+    } else {
+      toExecute = [actions.actions[0]];
+    }
+    for (const action of toExecute) {
+      log.debug(`executing an action ${action.type}`);
+      executeAction(action);
+    }
   }
 };
