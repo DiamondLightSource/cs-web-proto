555a8f996b716b38d62889ee8bff72e285ecc597
Will Rogers <will.rogers@diamond.ac.uk>
2019-12-17 13:39:29 +0000

    Rename App to app.
    This conforms with the filename convention and helps get around some
    difficulties with ignoring the rule.

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./app";
 
 it("renders without crashing", (): void => {
-  const div = document.createElement("div");
+  const div = app.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./app.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { getStore, initialiseStore } from "./redux/store";
