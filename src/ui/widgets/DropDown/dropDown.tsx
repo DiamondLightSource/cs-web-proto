0414d2226ba96aa84d32b72c317a84686e01b9b4
Tim Guite <tim.guite@diamond.ac.uk>
2020-03-09 10:58:02 +0000

    Added drop down component
    This looks quite nice and seems to fit in well with our setup

---
+++ src/ui/widgets/DropDown/dropDown.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import classes from "./dropDown.module.css";
+import { Widget } from "../widget";
+import { WidgetPropType } from "../widgetProps";
+import { registerWidget } from "../register";
+import {
+  BoolPropOpt,
+  StringProp,
+  InferWidgetProps,
+  ChildrenPropOpt,
+  FontPropOpt,
+  BorderPropOpt
+} from "../propTypes";
+
+const DropDownContainerProps = {
+  title: StringProp,
+  open: BoolPropOpt,
+  font: FontPropOpt,
+  border: BorderPropOpt,
+  children: ChildrenPropOpt
+};
+
+const DropDownWidgetProps = {
+  ...DropDownContainerProps,
+  ...WidgetPropType
+};
+
+export const DropDownComponent = (
+  props: InferWidgetProps<typeof DropDownContainerProps>
+): JSX.Element => (
+  <details
+    className={classes.Detail}
+    open={props.open ?? false}
+    style={{ ...props.font?.asStyle(), ...props.border?.asStyle() }}
+  >
+    <summary className={classes.Summary}>{props.title}</summary>
+    <div
+      className={classes.Children}
+      style={{ position: "relative", height: "100%", width: "100%" }}
+    >
+      {props.children}
+    </div>
+  </details>
+);
+
+type DropDownWidget = InferWidgetProps<typeof DropDownWidgetProps>;
+
+export const DropDown = (props: DropDownWidget): JSX.Element => (
+  <Widget baseWidget={DropDownComponent} {...props} />
+);
+
+registerWidget(DropDown, DropDownWidgetProps, "dropdown");
