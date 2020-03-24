253648a9d653a82d7657d0a2d5a4c34e1a55d987
Tim Guite <tim.guite@diamond.ac.uk>
2020-03-24 11:38:30 +0000

    Changed to demonstrate simple example, now need to add previous elements back in

--- src/ui/widgets/Slideshow/slideshow.tsx
+++ src/ui/widgets/Slideshow/slideshow.tsx
@@ -4,6 +4,7 @@ import log from "loglevel";
 import { CSSTransition, SwitchTransition } from "react-transition-group";
 
 import classes from "./slideshow.module.css";
+import fadeTransition from "./fade.module.css";
 import { Widget } from "../widget";
 import { WidgetPropType } from "../widgetProps";
 import { registerWidget } from "../register";
@@ -13,8 +14,6 @@ import {
   StringOrNumPropOpt
 } from "../propTypes";
 
-import "./styles.css";
-
 const SlideshowProps = {
   children: PropTypes.arrayOf(PropTypes.element),
   overflow: ChoicePropOpt(["scroll", "hidden", "auto", "visible"]),
@@ -22,97 +21,40 @@ const SlideshowProps = {
   maxWidth: StringOrNumPropOpt
 };
 
+const SwitchableText = (props: { index: number; children: any }) => {
+  return (
+    <SwitchTransition mode="out-in">
+      <CSSTransition
+        classNames={fadeTransition}
+        key={props.index}
+        timeout={1000}
+        unmountOnExit
+        mountOnEnter
+      >
+        {props.children[props.index]}
+      </CSSTransition>
+    </SwitchTransition>
+  );
+};
+
 export const SlideshowComponent = (
   props: InferWidgetProps<typeof SlideshowProps>
 ): JSX.Element => {
-  const nextChildIndex = (index: number, length: number): number => {
-    if (index + 1 < length) {
-      return index + 1;
-    } else {
-      return 0;
-    }
-  };
-  const previousChildIndex = (index: number, length: number): number => {
-    if (index - 1 >= 0) {
-      return index - 1;
-    } else {
-      return length - 1;
-    }
-  };
-
   const [childIndex, setChildIndex] = useState(0);
 
   log.warn(`Slideshow Index: ${childIndex}`);
   //   log.warn(props.children);
 
-  const SwitchableText = (props: { index: number; children: any }) => {
-    return (
-      <SwitchTransition mode="out-in">
-        <CSSTransition
-          classNames="alert"
-          key={props.index}
-          timeout={1000}
-          unmountOnExit
-          mountOnEnter
-        >
-          {props.children[props.index]}
-        </CSSTransition>
-      </SwitchTransition>
-    );
-  };
-
   return (
-    <div
-      className={classes.Slideshow}
-      style={{
-        maxWidth: props.maxWidth ?? "",
-        maxHeight: props.maxHeight ?? ""
-      }}
-    >
+    <div>
+      <h1>Click the button to transition the text below</h1>
+      <SwitchableText index={childIndex}>{props.children}</SwitchableText>
       <button
-        style={{
-          position: "relative",
-          height: "80%",
-          width: "10%",
-          maxWidth: "3rem",
-          maxHeight: "5rem",
-          textAlign: "center",
-          overflow: "hidden",
-          margin: "0.5rem",
-          flexGrow: 0
-        }}
         onClick={() =>
-          setChildIndex(
-            previousChildIndex(childIndex, props.children?.length ?? 0)
-          )
+          childIndex >= 2 ? setChildIndex(0) : setChildIndex(childIndex + 1)
         }
       >
-        ◀
-      </button>
-
-      <SwitchableText index={childIndex}>
-        <h1>This</h1>
-        <h1>Is</h1>
-        <h1>Great</h1>
-      </SwitchableText>
-
-      <button
-        style={{
-          position: "relative",
-          height: "80%",
-          width: "10%",
-          maxWidth: "3rem",
-          maxHeight: "5rem",
-          textAlign: "center",
-          overflow: "hidden",
-          margin: "0.5rem",
-          flexGrow: 0
-        }}
-        onClick={() =>
-          setChildIndex(nextChildIndex(childIndex, props.children?.length ?? 0))
-        }
-      >
-        ▶
+        toggle
       </button>
     </div>
   );
