34022bf9156bae6f1ec2be85cd59d25b4b9b8f49
Tim Guite <tim.guite@diamond.ac.uk>
2020-03-24 12:35:12 +0000

    Good working demo now!
    Would be nice if I could iron out the slight issue which occurs when you move from moving right to moving left. Still works quite well though!
    
    Appears to work for single child component as well!
    
    Some tests needed before merging.

--- src/ui/widgets/Slideshow/slideshow.tsx
+++ src/ui/widgets/Slideshow/slideshow.tsx
@@ -1,33 +1,40 @@
-import React, { useState, ReactChildren } from "react";
-import PropTypes from "prop-types";
+import React, { useState, ReactChildren, ReactElement } from "react";
+import PropTypes, { ReactElementLike } from "prop-types";
 import log from "loglevel";
 import { CSSTransition, SwitchTransition } from "react-transition-group";
 
 import classes from "./slideshow.module.css";
-import fadeTransition from "./fade.module.css";
+import slideLeftTransition from "./slideLeft.module.css";
+import slideRightTransition from "./slideRight.module.css";
 import { Widget } from "../widget";
 import { WidgetPropType } from "../widgetProps";
 import { registerWidget } from "../register";
 import {
   ChoicePropOpt,
   InferWidgetProps,
-  StringOrNumPropOpt
+  StringOrNumPropOpt,
+  BorderPropOpt
 } from "../propTypes";
 
 const SlideshowProps = {
   children: PropTypes.arrayOf(PropTypes.element),
   overflow: ChoicePropOpt(["scroll", "hidden", "auto", "visible"]),
   maxHeight: StringOrNumPropOpt,
-  maxWidth: StringOrNumPropOpt
+  maxWidth: StringOrNumPropOpt,
+  border: BorderPropOpt
 };
 
-const SwitchableText = (props: { index: number; children: any }) => {
+const SwitchableWidget = (props: {
+  index: number;
+  children: [ReactElement];
+  transition: { readonly [key: string]: string };
+}) => {
   return (
     <SwitchTransition mode="out-in">
       <CSSTransition
-        classNames={fadeTransition}
+        classNames={props.transition}
         key={props.index}
-        timeout={1000}
+        timeout={250}
         unmountOnExit
         mountOnEnter
       >
@@ -40,21 +47,91 @@ const SwitchableText = (props: { index: number; children: any }) => {
 export const SlideshowComponent = (
   props: InferWidgetProps<typeof SlideshowProps>
 ): JSX.Element => {
+  const nextChildIndex = (index: number, length: number): number => {
+    setTransition(slideRightTransition);
+    if (index + 1 < length) {
+      return index + 1;
+    } else {
+      return 0;
+    }
+  };
+  const previousChildIndex = (index: number, length: number): number => {
+    if (index - 1 >= 0) {
+      return index - 1;
+    } else {
+      return length - 1;
+    }
+  };
   const [childIndex, setChildIndex] = useState(0);
+  const [transition, setTransition] = useState(slideRightTransition);
 
   log.warn(`Slideshow Index: ${childIndex}`);
-  //   log.warn(props.children);
 
   return (
-    <div>
-      <h1>Click the button to transition the text below</h1>
-      <SwitchableText index={childIndex}>{props.children}</SwitchableText>
+    <div
+      className={classes.Slideshow}
+      style={{
+        maxWidth: props.maxWidth ?? "",
+        maxHeight: props.maxHeight ?? "",
+        ...(props.border?.css() ?? "")
+      }}
+    >
       <button
-        onClick={() =>
-          childIndex >= 2 ? setChildIndex(0) : setChildIndex(childIndex + 1)
-        }
+        style={{
+          position: "relative",
+          height: "80%",
+          width: "10%",
+          maxWidth: "3rem",
+          maxHeight: "5rem",
+          textAlign: "center",
+          overflow: "hidden",
+          margin: "0.5rem",
+          flexGrow: 0
+        }}
+        onClick={() => {
+          setTransition(slideLeftTransition);
+          setChildIndex(
+            previousChildIndex(childIndex, props.children?.length ?? 0)
+          );
+        }}
       >
-        toggle
+        ◀
+      </button>
+      <div
+        style={{
+          position: "relative",
+          width: "80%",
+          height: "100%",
+          maxHeight: props.maxHeight ?? "",
+          display: "flex",
+          flexGrow: 1,
+          overflow: props.overflow ?? ""
+        }}
+      >
+        <SwitchableWidget index={childIndex} transition={transition}>
+          {props.children as [ReactElement]}
+        </SwitchableWidget>
+      </div>
+      <button
+        style={{
+          position: "relative",
+          height: "80%",
+          width: "10%",
+          maxWidth: "3rem",
+          maxHeight: "5rem",
+          textAlign: "center",
+          overflow: "hidden",
+          margin: "0.5rem",
+          flexGrow: 0
+        }}
+        onClick={() => {
+          setTransition(slideRightTransition);
+          setChildIndex(
+            nextChildIndex(childIndex, props.children?.length ?? 0)
+          );
+        }}
+      >
+        ▶
       </button>
     </div>
   );
