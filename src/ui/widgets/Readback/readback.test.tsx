ad9c0dec029c91329db38e5e1b8f8e80210a5e56
Will Rogers <wahrogers@gmail.com>
2020-07-23 10:42:40 +0100

    React Testing Library tests for readback component.

--- src/ui/widgets/Readback/readback.test.tsx
+++ src/ui/widgets/Readback/readback.test.tsx
@@ -1,38 +1,45 @@
 import React from "react";
-import { ReadbackComponent, ReadbackComponentProps } from "./readback";
-import { configure, shallow, ShallowWrapper } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import { create, ReactTestRenderer } from "react-test-renderer";
-import { LabelComponent } from "../Label/label";
-import { DType } from "../../../types/dtypes";
+import { ReadbackComponent } from "./readback";
+import { DType, DDisplay } from "../../../types/dtypes";
+import { render } from "@testing-library/react";
 
-configure({ adapter: new Adapter() });
+describe("readback component", (): void => {
+  test("numeric precision", (): void => {
+    const readback = (
+      <ReadbackComponent
+        connected={true}
+        readonly={false}
+        value={
+          new DType({ stringValue: "3.14159265359", doubleValue: 3.1415926539 })
+        }
+        precision={2}
+      />
+    );
 
-let snapshot: ReactTestRenderer;
-let wrapper: ShallowWrapper<ReadbackComponentProps>;
-
-beforeEach((): void => {
-  const readback = (
-    <ReadbackComponent
-      connected={true}
-      readonly={false}
-      value={
-        new DType({ stringValue: "3.14159265359", doubleValue: 3.1415926539 })
-      }
-      precision={2}
-    />
-  );
-  wrapper = shallow(readback);
-  snapshot = create(readback);
-});
-
-describe("<Readback />", (): void => {
-  test("it matches the snapshot", (): void => {
-    expect(snapshot.toJSON()).toMatchSnapshot();
+    const { queryByText } = render(readback);
+    // Check for precision.
+    expect(queryByText("3.14")).toBeInTheDocument();
   });
+  test("string value with units", (): void => {
+    const readback = (
+      <ReadbackComponent
+        connected={true}
+        readonly={false}
+        value={
+          new DType(
+            { stringValue: "hello" },
+            undefined,
+            undefined,
+            new DDisplay({ units: "xyz" })
+          )
+        }
+        precision={2}
+        showUnits={true}
+      />
+    );
 
-  test("it applies precision to numbers", (): void => {
-    const labelComponent = wrapper.find(LabelComponent);
-    expect(labelComponent.props().text).toEqual("3.14");
+    const { queryByText } = render(readback);
+    // Check for precision.
+    expect(queryByText("hello xyz")).toBeInTheDocument();
   });
 });
