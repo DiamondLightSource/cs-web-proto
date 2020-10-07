fb36b4dc7640c16ef588ffd8f754ec6318467af7
Will Rogers <wahrogers@gmail.com>
2020-10-07 12:02:53 +0100

    Update Display tests to avoid mocking.

--- src/ui/widgets/Display/display.test.tsx
+++ src/ui/widgets/Display/display.test.tsx
@@ -1,41 +1,18 @@
 import React from "react";
-import { shallow, ShallowWrapper } from "enzyme";
-import { create, ReactTestRenderer } from "react-test-renderer";
 import { DisplayComponent } from "./display";
 import { Label } from "../Label/label";
 import { RelativePosition } from "../../../types/position";
-import { MacroMap } from "../../../types/macros";
+import { contextRender } from "../../../setupTests";
 
-let snapshot: ReactTestRenderer;
-let wrapper: ShallowWrapper;
-
-// Mock useSelector to return a 'global' macro map.
-jest.mock("react-redux", (): object => {
-  return {
-    useSelector: (): MacroMap => ({
-      A: "B",
-      C: "D"
-    }),
-    useDispatch: (): void => {}
-  };
-});
-
-beforeEach((): void => {
-  const display = (
-    <DisplayComponent id="id1">
-      <Label text="hello" position={new RelativePosition()} />
-    </DisplayComponent>
-  );
-  wrapper = shallow(display);
-  snapshot = create(display);
-});
+const display = (
+  <DisplayComponent id="id1">
+    <Label text="hello" position={new RelativePosition()} />
+  </DisplayComponent>
+);
 
 describe("<Display />", (): void => {
-  test("it matches the snapshot", (): void => {
-    expect(snapshot.toJSON()).toMatchSnapshot();
-  });
-
-  test("it renders a div", (): void => {
-    expect(wrapper.childAt(0).type()).toEqual("div");
+  const { queryByText } = contextRender(display);
+  test("it renders the label", (): void => {
+    expect(queryByText("hello")).toBeInTheDocument();
   });
 });
