2a44894696c73d736f9d7dd828ca64b5c2323bb7
George Sheppard <george.sheppard@diamond.ac.uk>
2020-10-08 15:01:34 +0100

    Modifying led to add properties found in cs-studio, scale property done

--- src/ui/widgets/LED/led.tsx
+++ src/ui/widgets/LED/led.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { Widget } from "../widget";
-import { InferWidgetProps } from "../propTypes";
+import {
+  InferWidgetProps,
+  BoolPropOpt,
+  StringPropOpt,
+  FloatPropOpt
+} from "../propTypes";
 import { PVComponent, PVWidgetPropType } from "../widgetProps";
 import { registerWidget } from "../register";
 import classes from "./led.module.css";
-import { DAlarm } from "../../../types/dtypes";
-import { LabelComponent } from "../Label/label";
+import { DAlarm, DType } from "../../../types/dtypes";
 import { getClass } from "../alarm";
 
-// No current props but left for easy addition later
-const LedProps = {};
+/**
+ * tooltip: text to display if middle click on the component
+ * rule: a user defined rule for when the led should trigger e.g. "x < 10"
+ * scale: a scaling factor for the led from it's default value
+ */
+const LedProps = {
+  tooltip: StringPropOpt,
+  rule: StringPropOpt,
+  scale: FloatPropOpt
+};
+
+const ruleToAlarm = (rule: string, value: DType): DAlarm => {
+  return DAlarm.MAJOR;
+};
 
 export type LedComponentProps = InferWidgetProps<typeof LedProps> & PVComponent;
 
@@ -20,12 +36,22 @@ export type LedComponentProps = InferWidgetProps<typeof LedProps> & PVComponent;
  * function and only extra props defined on LedProps need to be passed in as well
  */
 export const LedComponent = (props: LedComponentProps): JSX.Element => {
-  const { value, connected } = props;
+  const { value, connected, tooltip, rule, scale = 1.0 } = props;
 
-  const alarm = value?.getAlarm() || DAlarm.NONE;
+  let alarm = DAlarm.NONE;
+  // User defined rule takes priority over alarm on PV
+  if (rule) {
+    alarm = value ? ruleToAlarm(rule, value) : DAlarm.NONE;
+  } else {
+    alarm = value?.getAlarm() || DAlarm.NONE;
+  }
   const className = getClass(classes, connected, alarm.quality);
 
-  return <LabelComponent text="" className={className} />;
+  const scaleTransform = {
+    transform: `scale(${scale})`
+  };
+
+  return <div className={className} style={scaleTransform} />;
 };
 
 const LedWidgetProps = {
