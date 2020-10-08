66f43430b9b64c7d6be7fcf5f6424cbd9e3fa737
George Sheppard <george.sheppard@diamond.ac.uk>
2020-10-08 15:46:26 +0100

    Added tooltip like how it is in csstudio

--- src/ui/widgets/LED/led.tsx
+++ src/ui/widgets/LED/led.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { Widget } from "../widget";
-import {
-  InferWidgetProps,
-  BoolPropOpt,
-  StringPropOpt,
-  FloatPropOpt
-} from "../propTypes";
+import { InferWidgetProps, StringPropOpt, FloatPropOpt } from "../propTypes";
 import { PVComponent, PVWidgetPropType } from "../widgetProps";
 import { registerWidget } from "../register";
 import classes from "./led.module.css";
@@ -13,12 +8,10 @@ import { DAlarm, DType } from "../../../types/dtypes";
 import { getClass } from "../alarm";
 
 /**
- * tooltip: text to display if middle click on the component
  * rule: a user defined rule for when the led should trigger e.g. "x < 10"
  * scale: a scaling factor for the led from it's default value
  */
 const LedProps = {
-  tooltip: StringPropOpt,
   rule: StringPropOpt,
   scale: FloatPropOpt
 };
@@ -33,10 +26,12 @@ export type LedComponentProps = InferWidgetProps<typeof LedProps> & PVComponent;
  * Creates a small led icon which can change color depending on connection
  * and alarm type, css file defines these colours
  * @param props properties to pass in, these will be handled by the below LED
- * function and only extra props defined on LedProps need to be passed in as well
+ * function and only extra props defined on LedProps need to be passed in as well,
+ * to define some text explaining the meaning of the LED in different colours add a
+ * tooltip property in a json file containing a led
  */
 export const LedComponent = (props: LedComponentProps): JSX.Element => {
-  const { value, connected, tooltip, rule, scale = 1.0 } = props;
+  const { value, connected, rule, scale = 1.0 } = props;
 
   let alarm = DAlarm.NONE;
   // User defined rule takes priority over alarm on PV
@@ -45,6 +40,7 @@ export const LedComponent = (props: LedComponentProps): JSX.Element => {
   } else {
     alarm = value?.getAlarm() || DAlarm.NONE;
   }
+  alarm = DAlarm.MAJOR;
   const className = getClass(classes, connected, alarm.quality);
 
   const scaleTransform = {
