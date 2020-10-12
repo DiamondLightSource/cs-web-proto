262cc87c496e746cf537f13b99da1d69180358ce
George Sheppard <george.sheppard@diamond.ac.uk>
2020-10-12 10:20:33 +0100

    Modifying widget into an widget that displays an image with a label below it

--- src/ui/widgets/Symbol/symbol.tsx
+++ src/ui/widgets/Symbol/symbol.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, useContext } from "react";
+import React from "react";
 
 import { Widget } from "../widget";
 import { WidgetPropType } from "../widgetProps";
@@ -9,45 +9,25 @@ import {
   StringPropOpt
 } from "../propTypes";
 import { registerWidget } from "../register";
-import { BaseUrlContext } from "../../../baseUrl";
+import { ImageComponent } from "../Image/image";
+import { LabelComponent } from "../Label/label";
+import { GroupingContainerComponent } from "../GroupingContainer/groupingContainer";
 
 const SymbolProps = {
   src: StringProp,
   alt: StringPropOpt,
-  fill: BoolPropOpt
+  fill: BoolPropOpt,
+  width: StringPropOpt
 };
 
 export const SymbolComponent = (
   props: InferWidgetProps<typeof SymbolProps>
 ): JSX.Element => {
-  const baseUrl = useContext(BaseUrlContext);
-  let file = `img/${props.src}`;
-  if (!file.startsWith("http")) {
-    file = `${baseUrl}/${file}`;
-  }
-  let imageSize: any = undefined;
-  let overflow = "auto";
-  if (props.fill === true) {
-    imageSize = "100%";
-    overflow = "hidden";
-  }
-
-  const style: CSSProperties = {
-    overflow: overflow,
-    textAlign: "left"
-  };
-
   return (
-    <div style={style}>
-      <img
-        src={file}
-        alt={props.alt || undefined}
-        style={{
-          height: imageSize,
-          width: imageSize
-        }}
-      />
-    </div>
+    <GroupingContainerComponent name={"Grouping"}>
+      <ImageComponent {...props} />;
+      <LabelComponent />;
+    </GroupingContainerComponent>
   );
 };
 
