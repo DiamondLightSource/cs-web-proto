af260964b9073384faf8ea4b9a149981f0492374
Will Rogers <wahrogers@gmail.com>
2020-10-14 14:33:55 +0100

    Make widget exports alphabetical.

--- src/ui/widgets/index.ts
+++ src/ui/widgets/index.ts
@@ -1,6 +1,9 @@
 export { ActionButton } from "./ActionButton/actionButton";
 export { Display } from "./Display/display";
+export { DrawerWidget } from "./Drawer/drawer";
+export { DropDown } from "./DropDown/dropDown";
 export { DynamicPageWidget } from "./DynamicPage/dynamicPage";
+export { DynamicTabs } from "./Tabs/dynamicTabs";
 export { EmbeddedDisplay } from "./EmbeddedDisplay/embeddedDisplay";
 export { FlexContainer } from "./FlexContainer/flexContainer";
 export { GroupBox } from "./GroupBox/groupBox";
@@ -8,18 +11,15 @@ export { GroupingContainer } from "./GroupingContainer/groupingContainer";
 export { Image } from "./Image/image";
 export { Input } from "./Input/input";
 export { Label } from "./Label/label";
+export { LED } from "./LED/led";
 export { MenuButton } from "./MenuButton/menuButton";
 export { MenuMux } from "./MenuMux/menuMux";
 export { ProgressBar } from "./ProgressBar/progressBar";
 export { Readback } from "./Readback/readback";
 export { Shape } from "./Shape/shape";
 export { SlideControl } from "./SlideControl/slideControl";
-export { DropDown } from "./DropDown/dropDown";
 export { Slideshow } from "./Slideshow/slideshow";
-export { DrawerWidget } from "./Drawer/drawer";
 export { TabContainer } from "./Tabs/tabContainer";
-export { DynamicTabs } from "./Tabs/dynamicTabs";
-export { LED } from "./LED/led";
 
 // By importing this you ensure all the above widgets are imported
 // and thus registered.
