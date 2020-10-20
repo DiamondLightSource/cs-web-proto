6d84f00bdc4be23a7c742a9efa1b6ae2181514f3
George Sheppard <george.sheppard@diamond.ac.uk>
2020-10-20 11:19:25 +0100

    More tests including snapshots of polyline and symbol

--- src/ui/widgets/Symbol/symbol.test.tsx
+++ src/ui/widgets/Symbol/symbol.test.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import renderer, { ReactTestRenderer } from "react-test-renderer";
 import { SymbolComponent } from "./symbol";
 import { LabelComponent } from "../Label/label";
+import { DType } from "../../../types/dtypes";
 
 const SymbolRenderer = (symbolProps: any): ReactTestRenderer => {
   return renderer.create(<SymbolComponent {...symbolProps} readonly={true} />);
 };
 
+const fakeValue = new DType({ stringValue: "Fake value" });
+
 describe("properties are added to symbol", (): void => {
   test("label is shown if showLabel is true", (): void => {
     const symbolProps = {
       name: "test name",
-      showLabel: true
+      showLabel: true,
+      value: fakeValue
     };
 
     const testRenderer = SymbolRenderer(symbolProps);
@@ -24,7 +28,7 @@ describe("properties are added to symbol", (): void => {
       g: 255,
       r: 255
     });
-    expect(label.props.text).toBe("test name");
+    expect(label.props.text).toBe("Fake value");
 
     const tree = testRenderer.toTree();
     expect(tree?.props.showLabel).toBe(true);
@@ -44,4 +48,9 @@ describe("properties are added to symbol", (): void => {
       'No instances found with node type: "LabelComponent"'
     );
   });
+});
+
+test("matches snapshot", (): void => {
+  const testRenderer = SymbolRenderer({});
+  expect(testRenderer).toMatchSnapshot();
 });
