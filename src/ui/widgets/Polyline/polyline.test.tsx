804f96ecec4a292395f5d432a588f44c413df6f4
George Sheppard <george.sheppard@diamond.ac.uk>
2020-10-28 09:41:03 +0000

    Tests to check properties and match snapshot

--- src/ui/widgets/Polyline/polyline.test.tsx
+++ src/ui/widgets/Polyline/polyline.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import renderer, { ReactTestRenderer } from "react-test-renderer";
+import { render } from "@testing-library/react";
 import { PolylineComponent } from "./polyline";
 import { ShapeComponent } from "../Shape/shape";
 import { Color } from "../../../types/color";
@@ -10,22 +11,55 @@ const PolylineRenderer = (polylineProps: any): ReactTestRenderer => {
   );
 };
 
-test("matches snapshot", (): void => {
-  const testRenderer = PolylineRenderer({});
-  expect(testRenderer).toMatchSnapshot();
-});
+describe("<PolylineComponent />", (): void => {
+  test("matches snapshot", (): void => {
+    const { asFragment } = render(
+      <PolylineComponent
+        {...({
+          backgroundColor: Color.fromRgba(0, 255, 255),
+          width: 50,
+          lineWidth: 4,
+          rotationAngle: 45
+        } as any)}
+      />
+    );
 
-test("properties are added to polyline component", (): void => {
-  const polylineProps = {
-    width: 20,
-    lineWidth: 4
-  };
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-  const testRenderer = PolylineRenderer(polylineProps);
+  test("default properties are added to polyline component", (): void => {
+    const polylineProps = {
+      width: 20,
+      lineWidth: 4,
+      backgroundColor: Color.fromRgba(0, 255, 255)
+    };
 
-  const labelProps = testRenderer.root.findByType(ShapeComponent).props;
+    const testRenderer = PolylineRenderer(polylineProps);
 
-  expect(labelProps.shapeWidth).toBe("20px");
-  expect(labelProps.shapeHeight).toBe("4px");
-  expect(labelProps.backgroundColor).toEqual(Color.CYAN);
+    const shapeProps = testRenderer.root.findByType(ShapeComponent).props;
+
+    expect(shapeProps.shapeWidth).toBe("20px");
+    expect(shapeProps.shapeHeight).toBe("4px");
+    expect(shapeProps.backgroundColor.text).toEqual("rgba(0,255,255,255)");
+    expect(shapeProps.visible).toBe(true);
+  });
+
+  test("props override default properties", (): void => {
+    const polylineProps = {
+      width: 15,
+      lineWidth: 15,
+      backgroundColor: Color.fromRgba(0, 255, 255),
+      transparent: true,
+      rotationAngle: 45,
+      visible: false
+    };
+
+    const testRenderer = PolylineRenderer(polylineProps);
+
+    const shapeProps = testRenderer.root.findByType(ShapeComponent).props;
+
+    expect(shapeProps.backgroundColor.text).toBe(Color.TRANSPARENT.toString());
+    expect(shapeProps.shapeTransform).toBe("rotate(45deg)");
+    expect(shapeProps.visible).toBe(false);
+  });
 });
