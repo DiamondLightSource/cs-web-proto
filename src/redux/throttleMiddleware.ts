a9efd9956b94fc14449d5cec1c9c9d7ce31f560f
George Sheppard <george.sheppard@diamond.ac.uk>
2020-12-07 13:37:44 +0000

    Simplified throttleMiddleware

--- src/redux/throttleMiddleware.ts
+++ src/redux/throttleMiddleware.ts
@@ -3,34 +3,25 @@ import { MiddlewareAPI, Dispatch } from "redux";
 
 export class UpdateThrottle {
   private queue: Action[];
-  private started: boolean;
-  private updateMillis: number;
   public ready: boolean;
   constructor(updateMillis: number) {
     this.queue = [];
-    this.started = false;
-    this.updateMillis = updateMillis;
-    this.ready = true;
-    this.queueUpdate = this.queueUpdate.bind(this);
-    this.clearQueue = this.clearQueue.bind(this);
-    this.setReady = this.setReady.bind(this);
+    this.ready = false;
+    setInterval(() => (this.ready = true), updateMillis);
   }
 
-  public queueUpdate(action: Action): void {
+  public queueUpdate(action: Action, store: MiddlewareAPI): void {
     this.queue.push(action);
-    if (!this.started) {
-      setInterval(this.setReady, this.updateMillis);
-      this.started = true;
+    if (this.ready) {
+      this.sendQueue(store);
     }
   }
 
-  public setReady(): void {
-    this.ready = true;
-  }
-
-  public clearQueue(store: MiddlewareAPI): void {
-    store.dispatch({ type: VALUES_CHANGED, payload: [...this.queue] });
-    this.queue.length = 0;
+  public sendQueue(store: MiddlewareAPI): void {
+    if (this.queue.length > 0) {
+      store.dispatch({ type: VALUES_CHANGED, payload: [...this.queue] });
+      this.queue = [];
+    }
     this.ready = false;
   }
 }
@@ -39,13 +30,9 @@ export const throttleMiddleware = (updater: UpdateThrottle) => (
   store: MiddlewareAPI
   // next(action) returns the action, but in the case of a value being cached,
   // we don't call next(action) so return undefined.
-  // this makes the return value 'Action | undefined'
 ) => (next: Dispatch<Action>) => (action: Action): Action | undefined => {
   if (action.type === VALUE_CHANGED) {
-    updater.queueUpdate(action);
-    if (updater.ready) {
-      updater.clearQueue(store);
-    }
+    updater.queueUpdate(action, store);
   } else {
     return next(action);
   }
