186a36bc3e97132304db1917e300d1972b7081e9
Will Rogers <wahrogers@gmail.com>
2021-05-10 16:43:06 +0100

    LED widget behaves more like OPIBuilder.

--- src/ui/widgets/LED/led.tsx
+++ src/ui/widgets/LED/led.tsx
@@ -1,42 +1,36 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { Widget } from "../widget";
-import {
-  InferWidgetProps,
-  StringPropOpt,
-  FloatPropOpt,
-  BoolProp
-} from "../propTypes";
+import { InferWidgetProps, FloatPropOpt, ColorPropOpt } from "../propTypes";
 import { PVComponent, PVWidgetPropType } from "../widgetProps";
 import { registerWidget } from "../register";
 import classes from "./led.module.css";
-import { DAlarm } from "../../../types/dtypes";
 
 /**
  * width: the diameter of the LED
- * userColor: the color associated with the resolved value of the user defined rule
- *  (if there is one)
  */
 export const LedProps = {
   width: FloatPropOpt,
-  userColor: StringPropOpt,
-  alarmSensitive: BoolProp
+  onColor: ColorPropOpt,
+  offColor: ColorPropOpt
 };
 
 export type LedComponentProps = InferWidgetProps<typeof LedProps> & PVComponent;
 
 /**
- * Creates a small led icon which can change color depending on alarm type,
- * css file defines these colours
  * @param props properties to pass in, these will be handled by the below LED
  * function and only extra props defined on LedProps need to be passed in as well,
  * to define some text explaining the meaning of the LED in different colours add a
  * tooltip property in a json file containing a led
  */
 export const LedComponent = (props: LedComponentProps): JSX.Element => {
-  const { value, userColor, alarmSensitive, width } = props;
+  const { value, onColor, offColor, width } = props;
 
-  const style: any = {};
+  const style: CSSProperties = {};
 
+  style["backgroundColor"] = offColor?.toString();
+  if (value !== undefined && value?.getDoubleValue() !== 0) {
+    style["backgroundColor"] = onColor?.toString();
+  }
   if (width) {
     // make sizes similar to size in CS-Studio, five taken
     // away from default in css file too
@@ -44,21 +38,7 @@ export const LedComponent = (props: LedComponentProps): JSX.Element => {
     style.height = `${width - 5}px`;
   }
 
-  let allClasses = classes.Led;
-  // User defined rules take precedent over alarmSensitity
-  if ("rules" in props) {
-    style.backgroundColor = userColor ? userColor : "#00ff00";
-  } else {
-    if (alarmSensitive) {
-      const alarm = value?.getAlarm() || DAlarm.NONE;
-      const css = classes[alarm.quality];
-      if (css) {
-        allClasses += ` ${css}`;
-      }
-    }
-  }
-
-  return <div className={allClasses} style={style} />;
+  return <div className={classes.Led} style={style} />;
 };
 
 const LedWidgetProps = {
