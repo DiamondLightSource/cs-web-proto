a0a2df5085c300278e6221a9910cf7fc6e1f15b5
Will Rogers <wahrogers@gmail.com>
2021-05-21 17:04:43 +0100

    Change grouping container test to use React Testing Library.

--- src/ui/widgets/GroupingContainer/groupingContainer.test.tsx
+++ src/ui/widgets/GroupingContainer/groupingContainer.test.tsx
@@ -1,36 +1,24 @@
 import React from "react";
 import { GroupingContainerComponent } from "./groupingContainer";
-import { shallow, ShallowWrapper } from "enzyme";
-import { create, ReactTestRenderer } from "react-test-renderer";
-import { MacroContext } from "../../../types/macros";
+import { contextRender } from "../../../testResources";
+import { create } from "react-test-renderer";
 
-let snapshot: ReactTestRenderer;
-let wrapper: ShallowWrapper;
-
-beforeEach((): void => {
-  const grouping = <GroupingContainerComponent name={"Test"} />;
-  wrapper = shallow(grouping);
-  snapshot = create(grouping);
-});
+const grouping = <GroupingContainerComponent name={"Test"} />;
 
 describe("<GroupingContainerComponent />", (): void => {
   test("it matches the snapshot", (): void => {
+    const snapshot = create(grouping);
     expect(snapshot.toJSON()).toMatchSnapshot();
   });
 
-  test("it is a div HTML object", (): void => {
-    expect(wrapper.type()).toEqual(MacroContext.Provider);
-    expect(wrapper.childAt(0).type()).toEqual("div");
-  });
-
-  test("it renders child div with text", (): void => {
+  test("it renders child with text", (): void => {
     const childText = "Testing Child Component";
     const groupingWithChild = (
       <GroupingContainerComponent name={"Test"}>
         <div>{childText}</div>
       </GroupingContainerComponent>
     );
-    const wrapperWithChild = shallow(groupingWithChild);
-    expect(wrapperWithChild.childAt(0).text()).toEqual(childText);
+    const { getByText } = contextRender(groupingWithChild);
+    expect(getByText(childText)).toBeInTheDocument();
   });
 });
