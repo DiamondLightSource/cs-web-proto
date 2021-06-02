e367619a8617edb4cb96a5232cc0d0e25a317a62
Will Rogers <wahrogers@gmail.com>
2021-06-02 14:29:26 +0100

    Remove unneeded configuration.

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   transform: { "^.+\\.tsx?$": "ts-jest" },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  snapshotSerializers: ["enzyme-to-json/serializer"],
   setupTestFrameworkScriptFile: "<rootDir>/src/setupTests.tsx",
   collectCoverage: true,
   collectCoverageFrom: ["**/*.ts", "!<rootDir>/node_modules/**"],
