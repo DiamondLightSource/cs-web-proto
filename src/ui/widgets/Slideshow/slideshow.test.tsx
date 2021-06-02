97804ba99ff4927563f09b6fc1485143d18c9dee
Will Rogers <wahrogers@gmail.com>
2021-06-02 12:57:58 +0100

    Change slideshow tests to use React Testing Library.

--- src/ui/widgets/Slideshow/slideshow.test.tsx
+++ src/ui/widgets/Slideshow/slideshow.test.tsx
@@ -1,59 +1,45 @@
 import React from "react";
-import { mount, ReactWrapper } from "enzyme";
-import { create, ReactTestRenderer } from "react-test-renderer";
-import { InferWidgetProps } from "../propTypes";
+import { fireEvent, render, waitFor } from "@testing-library/react";
+import { create } from "react-test-renderer";
 
-import { SlideshowProps, SlideshowComponent } from "./slideshow";
+import { SlideshowComponent } from "./slideshow";
 
-let snapshot: ReactTestRenderer;
-let mounted: ReactWrapper<InferWidgetProps<typeof SlideshowProps>>;
-
-beforeEach((): void => {
-  const slideshow = (
-    <SlideshowComponent>
-      <div id="child-element">Child 1</div>
-      <div id="child-element">Child 2</div>
-      <div id="child-element">Child 3</div>
-    </SlideshowComponent>
-  );
-  snapshot = create(slideshow);
-  mounted = mount(slideshow);
-});
+const slideshow = (
+  <SlideshowComponent>
+    <div id="child-element">Child 1</div>
+    <div id="child-element">Child 2</div>
+    <div id="child-element">Child 3</div>
+  </SlideshowComponent>
+);
 
 describe("<Slideshow />", (): void => {
   test("it matches the snapshot", (): void => {
+    const snapshot = create(slideshow);
     expect(snapshot.toJSON()).toMatchSnapshot();
   });
 
   test("it renders only one child component", () => {
-    expect(mounted.find("#child-element")).toHaveLength(1);
-  });
-
-  test("it renders the first child on startup", () => {
-    expect(mounted.find("#child-element").text()).toEqual("Child 1");
-    expect(mounted.find("SwitchableWidget").prop("index")).toEqual(0);
+    const { queryByText } = render(slideshow);
+    expect(queryByText("Child 1")).toBeInTheDocument();
+    expect(queryByText("Child 2")).not.toBeInTheDocument();
   });
 
   // Using the index as I know this works and the transition seems to slow
   // down the actual rendering of the new div
-  test("next button iterates index", () => {
-    mounted.find("#next-button").simulate("click");
-    expect(mounted.find("SwitchableWidget").prop("index")).toEqual(1);
+  test("next button iterates index", async () => {
+    const { getByText, queryByText } = render(slideshow);
+    expect(queryByText("Child 2")).not.toBeInTheDocument();
+    const nextButton = getByText("▶");
+    fireEvent.click(nextButton);
+    // Wait for transition.
+    await waitFor(() => expect(queryByText("Child 2")).toBeInTheDocument());
   });
-  test("next button loops index on overflow", (): void => {
-    mounted.find("#next-button").simulate("click");
-    expect(mounted.find("SwitchableWidget").prop("index")).toEqual(1);
-    mounted.find("#next-button").simulate("click");
-    expect(mounted.find("SwitchableWidget").prop("index")).toEqual(2);
-    mounted.find("#next-button").simulate("click");
-    expect(mounted.find("SwitchableWidget").prop("index")).toEqual(0);
-  });
-  test("previous button loops around on underflow", (): void => {
-    mounted.find("#prev-button").simulate("click");
-    expect(mounted.find("SwitchableWidget").prop("index")).toEqual(2);
-    mounted.find("#prev-button").simulate("click");
-    expect(mounted.find("SwitchableWidget").prop("index")).toEqual(1);
-    mounted.find("#prev-button").simulate("click");
-    expect(mounted.find("SwitchableWidget").prop("index")).toEqual(0);
+  test("previous button loops to last index", async () => {
+    const { getByText, queryByText } = render(slideshow);
+    expect(queryByText("Child 3")).not.toBeInTheDocument();
+    const previousButton = getByText("◀");
+    fireEvent.click(previousButton);
+    // Wait for transition.
+    await waitFor(() => expect(queryByText("Child 3")).toBeInTheDocument());
   });
 });
